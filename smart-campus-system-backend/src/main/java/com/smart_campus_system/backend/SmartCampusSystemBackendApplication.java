5865aef59fa86bf285715d9952647a6e4607420e
Thirumurugan2730 <thiruveni776@gmail.com>
2026-01-26 12:53:14 +0530

    Frontend and Backend files are created

---
+++ smart-campus-system-backend/src/main/java/com/smart_campus_system/backend/SmartCampusSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smart_campus_system.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusSystemBackendApplication.class, args);
+	}
+
+}
---
+++ smart-campus-system-backend/src/test/java/com/smart_campus_system/backend/SmartCampusSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_campus_system.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
