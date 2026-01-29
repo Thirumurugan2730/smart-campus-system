87497030af034769cd00b7327d6f549695b7696a
Thirumurugan2730 <thiruveni776@gmail.com>
2026-01-29 05:52:07 +0530

    Booking Logic corrected

--- smart-campus-system-backend/src/main/java/com/smart_campus_system/backend/DTO/BookingRequestDTO.java
+++ smart-campus-system-backend/src/main/java/com/smart_campus_system/backend/DTO/BookingRequestDTO.java
@@ -9,4 +9,35 @@ public class BookingRequestDTO {
     public LocalDate bookingDate;
     public LocalTime startTime;
     public LocalTime endTime;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getResourceId() {
+		return resourceId;
+	}
+	public void setResourceId(int resourceId) {
+		this.resourceId = resourceId;
+	}
+	public LocalDate getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(LocalDate bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+    
 }
--- smart-campus-system-backend/src/main/java/com/smart_campus_system/backend/Service/BookingService.java
+++ smart-campus-system-backend/src/main/java/com/smart_campus_system/backend/Service/BookingService.java
@@ -7,15 +7,36 @@ import org.springframework.stereotype.Service;
 
 import com.smart_campus_system.backend.DTO.BookingRequestDTO;
 import com.smart_campus_system.backend.Entity.BookingEntity;
+import com.smart_campus_system.backend.Entity.BookingStatus;
+import com.smart_campus_system.backend.Entity.ResourcesEntity;
+import com.smart_campus_system.backend.Entity.UserEntity;
 import com.smart_campus_system.backend.Repository.BookingRepository;
+import com.smart_campus_system.backend.Repository.ResourceRepository;
+import com.smart_campus_system.backend.Repository.UserRepository;
 
 @Service
 public class BookingService {
 	@Autowired
 	BookingRepository repository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	ResourceRepository resourceRepository;
 	
 	public BookingEntity create(BookingRequestDTO dto){
-		return repository.save(dto);
+		UserEntity userEntity=userRepository.findById(dto.getUserId()).orElseThrow();
+		
+		ResourcesEntity resourceEntity=resourceRepository.findById(dto.resourceId).orElseThrow();
+		
+		BookingEntity current=new BookingEntity();
+		current.setUser(userEntity);
+		current.setResource(resourceEntity);
+		current.setBookingDate(dto.getBookingDate());
+		current.setStartTime(dto.getStartTime());
+		current.setEndTime(dto.getEndTime());
+		current.setStatus(BookingStatus.PENDING);
+		return repository.save(current);
+		
 	}
 	
 	public BookingEntity get(int id) {
